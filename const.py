9352e6aaf1445c184cc02d9174acd0b3a0155cab
oywino <oyvindo@compuserve.com>
2025-05-10 19:56:30 +0200

    Folder cleanup

--- const.py
+++
@@ -1,8 +0,0 @@
-"""Constants for the Eaton xComfort Bridge integration."""
-
-DOMAIN = "xcomfort_bridge"
-CONF_MAC = "mac_address"
-CONF_AUTH_KEY = "auth_key"
-CONF_IDENTIFIER = "identifier"
-CONF_DIMMING = "dimming"
-CONF_GATEWAYS = "gateways"
